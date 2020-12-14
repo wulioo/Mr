6a3552af3a5b072c8d604788e784e19bd8e3fef1
zdh <a541541000@163.com>
2020-12-14 18:34:59 +0800

    db

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask_migrate import Migrate, MigrateCommand
 from apps.user.moder import User
 from apps import create_app
 from flask_script import Manager
-
 from exts import db
 
 app = create_app()
@@ -15,8 +14,20 @@ manager.add_command('db', MigrateCommand)
 
 
 @manager.command
-def init():
+def create():
+    # --创建一个数据
+    db.create_all()
     print('初始化')
+
+
+# 为user表添加数据
+@manager.option('-u', '--username', dest="username")
+@manager.option('-p', '--password', dest="password")
+@manager.option('-k', '--phone', dest='phone')
+def insetDB(username, password, phone):
+    user = User(username=username, password=password, phone=phone)
+    db.session.add(user)
+    db.session.commit()
 
 
 if __name__ == '__main__':
